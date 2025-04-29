cd772899707e06eeccb9115530342a225073437c
larasr <74631618+larasr@users.noreply.github.com>
2025-04-29 14:22:12 -0300

    Update load_data.sql
    Omitting the local path from user

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,2 +1,2 @@
 -- Execução realizada via psql tool workspace
-\copy sales_data (date, day, month, year, customer_age, age_group, customer_gender, country, state, product_category, sub_category, product, order_quantity, unit_cost, unit_price, profit, total_cost, total_price) FROM 'C:\Users\laras\Documents\Projetos\sales_data_analysis\data\cleaned_data.csv' WITH (DELIMITER ';', FORMAT csv, HEADER true);
+\copy sales_data (date, day, month, year, customer_age, age_group, customer_gender, country, state, product_category, sub_category, product, order_quantity, unit_cost, unit_price, profit, total_cost, total_price) FROM 'C:\path\sales_data_analysis\data\cleaned_data.csv' WITH (DELIMITER ';', FORMAT csv, HEADER true);
